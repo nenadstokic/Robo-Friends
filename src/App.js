3ffe14dc2aba661bb99510c776064ccfbd693cfd
Nenad <nstokic@gmail.com>
2019-05-27 12:19:41 +0200

    added fetching users from api

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({robots: robots});
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
   }
 
   render(){
